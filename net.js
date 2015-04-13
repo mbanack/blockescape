97cdad9d80510ebeebb599453ba0129d4693e487
Matt Banack <matt@banack.net>
2015-04-13 00:49:40 -0700

    net func stubs

--- net.js
+++ net.js
@@ -1,5 +1,16 @@
 (function(window, document, undefined){
+    net_open_socket = function() {
+
+    };
+
     net_init = function() {
         console.log("net_init");
+
+        console.log("TODO: repeated setTimeout");
     }();
+
+    // get current gamestate as json
+    net_get_gamestate = function() {
+
+    };
 }());
