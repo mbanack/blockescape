2eb58cd4c38f7b8a0e7f592b542b730f33f9af34
dtran042 <dtran042@ucr.edu>
2015-04-21 20:25:35 -0700

    FUCK YESSSSSS

--- ocanvas/anim.js
+++ ocanvas/anim.js
@@ -1,95 +1,110 @@
 function init() {
   //working on the file on git?
   var rectangle;
+  var rectangle2;
+  var rectangle_array = [];  
+  var const_array = [];
+  var counter = 0;
+  var string = "lalalala";
+  var string2 = "";
+
   //Block 1
   var canvas = oCanvas.create({
     canvas: "#canvas",
     background: "#ccc"
   });
  
-  //Block 2
-  var quadrado = canvas.display.rectangle({
-    x: 25,
-    y: 25,
-    width: 20,
-    height: 20, 
-    fill: "#0aa",
-    velocX: 8,
-    velocY: 8
-  });
-  canvas.addChild(quadrado);
+  function make_rect(xpos,ypos,w,h){
+    var temp_rect = canvas.display.rectangle({
+      x: xpos,
+      y: ypos,
+      width: w,
+      height: h,
+      fill: "#f1f"
+    });
+    temp_rect.bind("mouseenter", function(){canvas.mouse.cursor("pointer"); string2 = "now in rect" + temp_rect.id; string = "restrict y to "+getYOfId(temp_rect.id);})
+                       .bind("mouseleave", function() {canvas.mouse.cursor("default"); string2 = ""});
+    rectangle_array.unshift(temp_rect);
+    const_array.unshift(ypos);
+    counter++;
+  }
 
-function init_rect(rectangle_temp,xpos,ypos,rw,rh){
-    rectangle_temp = canvas.display.rectangle({
-    x: xpos,
-    y: ypos,
-    width: rw,
-    height: rh, 
-    fill: "#f1f",
-  });
-  canvas.addChild(rectangle_temp);
-  return rectangle_temp;
-}
+  make_rect(270,50,100,50);
+  make_rect(270,150,100,50);
+  make_rect(270,250,100,50);
+  make_rect(270,350,100,50);
 
-rectangle = init_rect(rectangle,100,350,100,50);
-const temp_y = rectangle.y; //lock movement of rectangle to its current y position
-  //Block 3
-  canvas.bind("click tap", function() {
-    quadrado.x = canvas.mouse.x;
-    quadrado.y = canvas.mouse.y;
-  });
-
-  rectangle.dragAndDrop({
-    start: function() {
-      this.fill = "ff0";
-    },
-    move: function() {
-      this.fill = "0f0";
-      this.y = temp_y; //restrict to horizontal movement only
-      if(this.x <= 0) //restrict is now tested on movement rather than snap after mouse release
-        this.x = 0;
-      if((rectangle.x+rectangle.width) >= (canvas.width))
-        rectangle.x = (canvas.width - rectangle.width);
-    },
-    end: function() {
-      this.fill = "f00";
-      //this.y = 400; //snap it to this location after release mouse
+  function getYOfId(temp_id)
+  {
+    for(i = 0; i < rectangle_array.length; i++)
+    {
+      if(rectangle_array[i].id == temp_id)
+        return rectangle_array[i].y;
     }
+    return -1;
+  }
+
+
+  // for(j = const_array.length-1; j >= 0 ; j--)
+  // {
+  //   string = string + " " + const_array[j];
+  // }
+  // for(j = 0; j < const_array.length; j++)
+  // {
+  //   string = string + " " + const_array[j];
+  // }
+  
+  var flag = true;
+  var yholder;
+  
+  for(i = 0; i < rectangle_array.length; i++)
+  {
+    canvas.addChild(rectangle_array[i]);
+    
+    rectangle_array[i].dragAndDrop({
+      start: function() {
+        this.fill = "ff0";
+      },
+      move: function() {
+        this.fill = "0f0";
+        if(flag == true) 
+          yholder = getYOfId((rectangle_array.length-i));
+        flag = false;
+        this.y = yholder; //restrict to horizontal movement only
+        if(this.x <= 0) //restrict is now tested on movement rather than snap after mouse release
+          this.x = 0;
+        if((rectangle_array[0].x+rectangle_array[0].width) >= (canvas.width))
+          rectangle_array[0].x = (canvas.width - rectangle_array[0].width);
+      },
+      end: function() {
+       this.fill = "f00";
+       flag = true;
+        //this.y = 400; //snap it to this location after release mouse
+      }
+    });
+  }
+
+  var text = canvas.display.text({
+    x: 177,
+    y: 196,
+    origin: { x: "center", y: "center" },
+    align: "center",
+    font: "bold 12px/1.5 sans-serif",
+    text: "Mouse Position\nX: 0\nY: 0",
+    fill: "#000"
+  });
+  canvas.addChild(text);
+
+  // //Block 4
+  canvas.setLoop(function() {  
+    text.text = "Mouse Position\n" +
+      "X: " + canvas.mouse.x + "\n" +
+      "Y: " + canvas.mouse.y + "\n" + 
+      rectangle_array.length + "\n" + 
+      string + "\n" + string2
   });
 
-rectangle.bind("mouseenter", function(){canvas.mouse.cursor("pointer");})
-         .bind("mouseleave", function() {canvas.mouse.cursor("default");});
 
-var text = canvas.display.text({
-  x: 177,
-  y: 196,
-  origin: { x: "center", y: "center" },
-  align: "center",
-  font: "bold 25px/1.5 sans-serif",
-  text: "Mouse Position\nX: 0\nY: 0",
-  fill: "#000"
-});
-canvas.addChild(text);
-
-
-  //Block 4
-  canvas.setLoop(function() {
-    quadrado.x += quadrado.velocX;
-    quadrado.y += quadrado.velocY;
-    quadrado.rotation++;    
-    if ((quadrado.x <= 0) || (quadrado.x >= (canvas.width)))  
-      quadrado.velocX = -quadrado.velocX;
-    if ((quadrado.y < 20) || (quadrado.y > (canvas.height - 20))) 
-      quadrado.velocY = -quadrado.velocY;
-    
-  text.text = "Mouse Position\n" +
-      "X: " + canvas.mouse.x + "\n" +
-      "Y: " + canvas.mouse.y + "\n" +
-      "rectangle.x = " + rectangle.x + "\n" +
-      "rectangle.y = " + rectangle.y + "\n" +
-      "temp_y = " + temp_y + "\n";
-  }).start();
-
-
+  canvas.timeline.start();
 
 }
