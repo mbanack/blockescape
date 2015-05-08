28146e73bdd71e638e84eb3191547cf3c23b019e
--gloabal <jmine001@ucr.edu>
2015-05-08 13:04:19 -0700

    Merge my board.cpp

--- gameserver/inc/Auth.hpp
+++ gameserver/inc/Auth.hpp
@@ -1,5 +1,9 @@
 #ifndef AUTH_HPP_
 #define AUTH_HPP_
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 #include <string>
 #include <vector>
 class Auth {
@@ -13,5 +17,12 @@ public:
         const std::string &password);
     std::string getSalt(const std::string &username);
     std::string genSalt();
+    static Auth *getInstance();
+    ~Auth();
+private:
+    Auth();
+    static Auth *instance;
+    sql::Driver *driver;
+    sql::Connection *con;
 };
 #endif
